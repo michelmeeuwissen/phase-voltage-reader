ecbc11b045f783a0be445c742317474a88749f98
Michel Meeuwissen <mmeeuw@gmail.com>
2022-05-07 10:30:24 +0200

    init

---
+++ src/main/java/nl/tibs/phasevoltagereader/PhaseVoltageReaderApplication.java
@@ -0,0 +1,13 @@
+package nl.tibs.phasevoltagereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseVoltageReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaseVoltageReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/tibs/phasevoltagereader/PhaseVoltageReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tibs.phasevoltagereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaseVoltageReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
